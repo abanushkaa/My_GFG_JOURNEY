cd2067dca5406b806c313e72aa81c38bbf0d3b77
Anushka Baranwal <141651897+abanushkaa@users.noreply.github.com>
2025-08-29 23:30:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learning Structs/learning-structs.cpp
@@ -0,0 +1,10 @@
+/*structure node of linked list
+  to store data and link
+  to the next node
+*/
+struct Node {
+    // Your code here
+    int data;
+   struct Node *next;
+    // Linked list has data and next members
+};
