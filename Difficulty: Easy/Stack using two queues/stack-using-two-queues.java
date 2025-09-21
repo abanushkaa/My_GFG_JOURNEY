69a303d5299e650be2267c9d82acfbf964c713b0
Anushka Baranwal <141651897+abanushkaa@users.noreply.github.com>
2025-09-21 08:50:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stack using two queues/stack-using-two-queues.java
@@ -0,0 +1,25 @@
+class Queues {
+    Queue<Integer> q1 = new LinkedList<Integer>();
+    Queue<Integer> q2 = new LinkedList<Integer>();
+
+    void push(int a) {
+        // code here
+        q2.add(a);
+        while(!q1.isEmpty()){
+            q2.add(q1.remove());
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+        
+    }
+//
+    int pop() {
+        if(q1.isEmpty()){
+            return -1;
+        }
+        return q1.remove();
+        
+        // code here
+    }
+}
