daa182eeca9ccfc5aad606885cab89d3f5429790
Anushka Baranwal <141651897+abanushkaa@users.noreply.github.com>
2026-01-02 03:57:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int findSum(int n) {
+        // code here
+        if(n==0){
+            return 0;
+        }
+        int res= n*(n+1);
+        int result= res/2;
+        return result;
+    }
+}
